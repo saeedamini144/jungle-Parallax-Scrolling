8c324edb5cf0167d1abb21b9b64ce9d20e383e4f
Vahid HPS <vahidvb@live.com>
2023-12-28 12:12:01 +0330

    add effect to tree and text + some css changes

--- scripts.js
+++ scripts.js
@@ -3,17 +3,21 @@ let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+let tree = document.getElementById('tree');
 
 /////////arrow function pro/////////
 
 window.addEventListener('scroll' , () => {
     let value = window.scrollY;
     text.style.marginTop = value * 2.5 + "px";
+    text.style.letterSpacing = value * 0.05 + "px";
+    text.style.filter = "blur("+value * 0.03 + "px)";
     leaf.style.top = value * -1.5 + "px";
     leaf.style.left = value * 1.5 + "px";
     hill5.style.left = value * 0.5 + "px";
     hill4.style.left = value * -0.5 + "px";
     hill1.style.top = value * -0.3 + "px";
+    tree.style.scale = 1 + (value * -0.0005);
 });
 
 /////////Normal function/////////
